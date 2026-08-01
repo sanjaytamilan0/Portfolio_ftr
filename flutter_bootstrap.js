6bb542f42281e08810921f6a938782390ede99c4
sanjaytamilan0 <sanjaytamilan0@gmail.com>
2026-08-01 12:17:38 +0530

    Deploy to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "502597964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2605552770" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
